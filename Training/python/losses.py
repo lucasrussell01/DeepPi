ef811c9bf2fe7725341df229494cd3af4ed9b945
lucasrussell01 <lucascharlesrussell@gmail.com>
2022-08-15 15:01:25 +0100

    Add end of epoch checkpoint

--- Training/python/losses.py
+++ Training/python/losses.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras.callbacks import Callback
 
 class TauLosses:
 
@@ -8,3 +9,11 @@ class TauLosses:
         loss = tf.keras.losses.categorical_crossentropy(target, output)
         return loss
 
+class EpochCheckpoint(Callback):
+    def __init__(self, file_name_prefix):
+        self.file_name_prefix = file_name_prefix
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.model.save('{}_e{}.tf'.format(self.file_name_prefix, epoch),
+                        save_format="tf")
+        print("Epoch {} is ended.".format(epoch))
